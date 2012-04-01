ddbc760d7772b9eb3d8096cdddd9699a5685c9e0
Chach Sikes <chachasikes@gmail.com>
2012-04-01 09:53:17 -0700

    Loading JSON file into mongo and printing it out

--- api/loadfoods.php
+++ api/loadfoods.php
@@ -8,6 +8,27 @@ $collection = $m->openfood->foods;
 $cursor = $collection->find();
 
 
+// add another record, with a different "shape"
+$obj = array( "title" => "title", "value" => true );
+
+$file_path = '../data/open_food_bkg_colors.json';
+$file_data = file_get_contents($file_path);
+/* var_dump($file_data); */
+
+$json = json_decode($file_data);
+$objects = $json->foods;
+
+foreach ($objects as $obj_load) {
+  $collection->insert($obj_load);
+}
+
+
+
+// find everything in the collection
+$cursor = $collection->find();
+
+
+// Print data
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
 header("Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . "GMT"); 
 header("Cache-Control: no-cache, must-revalidate"); 
